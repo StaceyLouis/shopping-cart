4a39aafe4d130ef11d3de1c00ec060b1df7ae2b3
Stacey Louis <stacey-louis@lambdastudents.com>
2022-04-01 20:24:31 -0400

    added top banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import TopBanner from "./components/TopBanner";
+import BigNav from "./components/BigNav";
+import Blog from "./components/Blog";
+import BottomBanner from "./components/BottomBanner";
+import Featured from "./components/Featured";
+import Footer from "./components/Footer";
+import NewArrivals from "./components/NewArrivals";
+import Popular from "./components/Popular";
+import Shopping from "./components/Shopping";
+import SmallNav from "./components/SmallNav";
+
+import './index.css'
 
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-     App
+    <div>
+      <TopBanner/>
+      <SmallNav/>
+      <BigNav/>
+      <Featured/>
+      <Shopping/>
+      <Popular/>
+      <NewArrivals/>
+      <BottomBanner/>
+      <Blog/>
+      <Footer/>
+
     </div>
   );
 }
-
-export default App;
---
+++ src/components/TopBanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import  '../styles/TopBanner.css'
+
+const Banner = () => {
+  return (
+    <div className='top-banner'>
+      <p id="banner-desc"><span id="limited">Limited Time Only</span>Rewards Triple Points on Bras <a href="#" id="ban-link">Shop</a></p>
+    </div>
+  )
+}
+
+export default Banner
